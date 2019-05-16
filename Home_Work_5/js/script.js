85dd2b83670518b881604e2cb2f30e7774342fba
stalkervr <stalkervr@gmail.com>
2019-05-16 21:00:13 +0700

    Refactoring

--- Home_Work_5/js/script.js
+++ Home_Work_5/js/script.js
@@ -1,31 +1,4 @@
 
-window.onload = function() {
 
-    let menuItem = document.querySelectorAll('.menu-item'),
-        title = document.getElementById('title'),
-        adv = document.querySelector('.adv'),
-        prompte = document.getElementById('prompt'),
-        menu = document.querySelector('.menu'),
-        newItem = document.createElement('li'),
-        column = document.querySelectorAll('.column');
-
-        // console.log(menuItem);
-        // console.log(menu);
-        // console.log(title);
-        // console.log(adv);
-        // console.log(prompte);
-        // console.log(newItem);
-        // console.log(column);
- 
-        document.body.style.background ='url(../img/apple_true.jpg) center no-repeat';
-        menu.replaceChild(menuItem[1], menuItem[2]);
-        menu.insertBefore(menuItem[2], menuItem[1]);
-        newItem.classList.add('menu-item');
-        newItem.textContent = 'Пятый пункт';
-        menu.appendChild(newItem);
-        column[1].removeChild(adv);
-        title.textContent = 'Мы продаем только подлинную технику Apple';
-        prompte.textContent = prompt('Как вы относитесь к технике Apple ?', '');
-};
 
     
