56d7e2ca71b2a36fe462577674e5db2dd7493315
stalkervr <stalkervr@gmail.com>
2019-05-24 07:53:37 +0700

    transfer of form data to the server

--- Task_13/js/script.js
+++ Task_13/js/script.js
@@ -188,5 +188,42 @@ window.addEventListener('DOMContentLoaded', function () {
     });
     /* #endregion */
 
+    // Task_13 Form
+// TODO: Install local server for this task.  
+let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо скоро мы свами свяжемся',
+    failure: 'Что-то пошло не так'
+};
 
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+    // обработчик события необходимо вешать на саму форму!!!! а не на
+    // какую-либо конкретную форму!!!!
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4 ) {
+                statusMessage.innerHTML = message.loading;
+            }
+            else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            }
+            else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+    });
 });
